5f036af0981c554ac375ef8ea45bd71ede6f34bc
mellotonio <antoniombabo@uol.com.br>
2021-11-20 23:39:28 -0500

    feat: invoice and transfer integration

---
+++ starkbank.js
@@ -0,0 +1,18 @@
+const starkbank = require('starkbank');
+
+let privateKeyContent = `
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIPPTQnn7g+8DVhOcvqKxLrPFZ/hb0Ruc91amqiLruUESoAcGBSuBBAAK
+oUQDQgAExK3pY/lsTcl+RqdRV2JVghiNEbEzFIsmJS4PcC5vjGLCLSw3Q0RLj2NH
+JyS9YsVnOU5orR7RI4SY0FiEo4Hp3A==
+-----END EC PRIVATE KEY-----
+`
+
+let user = new starkbank.Project({
+    environment: 'sandbox',
+    id: '5642664049377280',
+    privateKey: privateKeyContent
+});
+starkbank.user = user;
+
+module.exports = starkbank;
---
+++ transfer.js
@@ -0,0 +1,26 @@
+const starkbank = require("./starkbank");
+
+class Transfer {
+    async Create(amount) {
+        try {
+            await starkbank.transfer.create([
+                {
+                    amount: amount,
+                    bankCode: '20018183',
+                    branchCode: '0001',
+                    accountNumber: '6341320293482496',
+                    taxId: '20.018.183/0001-80',
+                    name: 'Stark Bank S.A',
+                    accountType: 'payment',
+                }
+            ]); 
+        } catch (error) {
+            console.log("Error while creating transfer: ", error)
+        }
+    }
+}
+
+module.exports = Transfer;
+
+
+
